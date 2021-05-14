e6ba6b5c6693fcd9e7e24b52545d5b0c954154aa
CC Webb <thewebbdesigns@gmail.com>
2021-05-14 16:49:29 -0600

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,21 @@
 
 //list for class weeks to automatically update - hopefully
 
+const links = [
+    {label: "Week 01", url: "week1/index.html"},
+    {label: "Week 02", url: "week2/index.html"},
+    {label: "Week 03", url: "week3/week3.html"},   
+    {label: "Week 04", url: "week4/week4.html"},
+    {label: "Week 05", url: "week5/week05.html"},
+    {label: "Week 06", url: "week6/wwek6.html"},
+    {label: "Week 07", url: "week7/week7.html"},
+    {label: "Week 08", url: "week8/week8.html"},
+    {label: "Week 09", url: "week9/week9.html"},
+    {label: "Week 10", url: "week10/week10.html"},
+    {label: "Week 11", url: "week11/week11.html"},
+    {label: "Week 12", url: "week12/week12.html"},
+]
+
 const btn = document.getElementById('button');
 const rainbow = ['red','orange','yellow','green','blue','rebeccapurple','violet'];
 function change() {      
